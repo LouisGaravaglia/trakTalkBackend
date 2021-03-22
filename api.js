d9e416537171bb0f52ae3100c9de0408baa57bd6
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-03-22 15:04:48 -0700

    refactored to utilize heroku

--- api.js
+++ api.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
-const router = express.Router()
+// const router = express.Router()
 const expressWs = require('express-ws')(app);
 app.use(express.static(__dirname + '/public'));
 const serverless = require('serverless-http');
 // import ChatUser from "./chatuser.js"
+const PORT = process.env.PORT || 3000;
 
-router.get('/test', (req, res) => res.send('Hello World From Express Server'))
+app.get('/test', (req, res) => res.send('Hello World From Express Server'))
 
 // const ChatUser = require('./ChatUser');
 const {ChatUser} = require('./chatuser');
 
-router.ws('/chat/:roomName', function(ws, req, next) {
+app.ws('/chat/:roomName', function(ws, req, next) {
     console.log('hit chat in backend');
 
     try {
@@ -43,8 +44,8 @@ router.ws('/chat/:roomName', function(ws, req, next) {
   });
 
   // app.use('/', router)
-  app.use('/.netlify/functions/api', router)
-  // app.listen(3000, () => console.log('listening on port: 3000'))
+  // app.use('/.netlify/functions/api', router)
+  app.listen(PORT, () => console.log(`listening on port: ${PORT}`))
 
-module.exports.handler = serverless(app);
+// module.exports.handler = serverless(app);
 
