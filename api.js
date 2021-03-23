66b66b4f5e68cddfe59364e133c9192fc5879ff6
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-03-22 17:05:50 -0700

    conditional for displaying user signout to room

--- api.js
+++ api.js
@@ -1,28 +1,20 @@
 const express = require('express');
 const app = express();
-// const router = express.Router()
-const expressWs = require('express-ws')(app);
-app.use(express.static(__dirname + '/public'));
-const serverless = require('serverless-http');
-// import ChatUser from "./chatuser.js"
 const PORT = process.env.PORT || 3000;
-
-app.get('/test', (req, res) => res.send('Hello World From Express Server'))
-
-// const ChatUser = require('./ChatUser');
 const {ChatUser} = require('./chatuser');
+
+app.use(express.static(__dirname + '/public'));
+app.get('/test', (req, res) => res.send('Hello World From Express Server'))
 
 app.ws('/chat/:roomName', function(ws, req, next) {
     console.log('hit chat in backend');
 
     try {
       const user = new ChatUser(
-        ws.send.bind(ws), // fn to call to message this user
-        req.params.roomName // name of room for user
+        ws.send.bind(ws), 
+        req.params.roomName 
       );
-  
-      // register handlers for message-received, connection-closed
-  
+    
       ws.on('message', function(data) {
         try {
           user.handleMessage(data);
@@ -33,7 +25,7 @@ app.ws('/chat/:roomName', function(ws, req, next) {
   
       ws.on('close', function() {
         try {
-          user.handleClose();
+          if (user.name !== null) user.handleClose();
         } catch (err) {
           console.error(err);
         }
@@ -43,9 +35,5 @@ app.ws('/chat/:roomName', function(ws, req, next) {
     }
   });
 
-  // app.use('/', router)
-  // app.use('/.netlify/functions/api', router)
   app.listen(PORT, () => console.log(`listening on port: ${PORT}`))
-
-// module.exports.handler = serverless(app);
 
